5907d6de035ad8a5099266d63e6fcb23de2469fa
Josh Mackus <josh.mackus@ceridian.com>
2024-08-24 13:17:39 +1000

    feat: implement sidebars and parent layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,35 @@
+import HeaderBox from "@/components/HeaderBox";
+import RightSideBar from "@/components/RightSideBar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import Link from "next/link";
 import React from "react";
+import Image from "next/image";
 
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = {
+    firstName: "Josh",
+    lastName: "Mackus",
+    email: "myemail@email.com",
+  };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access your account information below."
+          />
+        </header>
+      </div>
+      <RightSideBar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.5 }, { currentBalance: 55.5 }]}
+      />
+    </section>
+  );
 };
 
 export default Home;
